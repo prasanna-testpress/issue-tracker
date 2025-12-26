0e621ae1e0d29f42fd2f8e436e871f820a537f9b
prasanna-testpress <prasanna@testpress.in>
2025-12-26 11:18:18 +0530

    feat: Implement custom user model and configure it in Django settings.

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+
+    list_display = (
+        'first_name',
+        'last_name',
+        'email',
+        'is_staff',
+        'is_active',
+        'created_at',
+    )
+
+    ordering = ('email',)
+
+    fieldsets = (
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        ('Permissions', {
+            'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Important Dates', {
+            'fields': ('last_login', )
+        }),
+    )
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,54 @@
+from enum import unique
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+     
+    def create_user(self, email,password):
+
+        if not email:
+            raise ValueError('Email must require for the user')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+
+        return user
+
+        
+    def create_superuser(self, email,password=None):
+
+        user = self.create_user(email,password)
+        
+        user.is_staff=True
+        user.is_superuser = True
+        user.save(using = self._db)
+
+        return user
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+
+    email = models.EmailField(max_length=100, unique=True)
+    first_name= models.CharField(max_length=50)
+    last_name= models.CharField(max_length=50)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
+
+    objects=UserManager()
+
+    class Meta:
+
+        ordering = ('-created_at',)
+
+    def __str__(self):
+
+        return self.email
